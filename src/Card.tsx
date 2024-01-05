f6997f4512ef36034a557cee51f17215c564f58c
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2024-01-05 01:24:08 -0300

    reconocer zona donde se mueve el cursor

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useRef, useState, MouseEvent } from "react";
 
 interface Data {
     name: string;
@@ -13,15 +13,100 @@ interface Props {
 
 const Card: FunctionComponent<Props> = ({ data }) => {
     const { name, number, image, type } = data;
+    const CARD = useRef<HTMLDivElement>(null);
+    const POINTER = useRef<HTMLDivElement>(null);
     const [active, setActive] = useState<boolean>(false);
 
+    const listener = (ev: MouseEvent) => {
+        if (active && CARD.current) {
+            const { clientY: Y, clientX: X } = ev;
+            const { offsetTop, offsetLeft, offsetHeight, offsetWidth } = CARD.current;
+            const anchorX = offsetLeft + offsetWidth / 2;
+            const anchorY = offsetTop + offsetHeight / 2;
+
+            const margin = 75;
+            const centerY = Y <= anchorY + margin && Y >= anchorY - margin;
+            const centerX = X <= anchorX + margin && X >= anchorX - margin;
+
+            // POINTER.current.style.top = ev.clientY + "px";
+            // POINTER.current.style.left = ev.clientX + "px";
+
+            console.clear();
+            if (centerY && centerX) {
+                console.log(`
+                · · ·
+                · + ·
+                · · ·`);
+            } else if (Y < anchorY && X < anchorX) {
+                console.log(`
+                + · ·
+                · · ·
+                · · ·`);
+            } else if (Y < anchorY && centerX) {
+                console.log(`
+                · + ·
+                · · ·
+                · · ·`);
+            } else if (Y < anchorY && X > anchorX) {
+                console.log(`
+                · · +
+                · · ·
+                · · ·`);
+            } else if (centerY && X < anchorX) {
+                console.log(`
+                · · ·
+                + · ·
+                · · ·`);
+            } else if (centerY && X > anchorX) {
+                console.log(`
+                · · ·
+                · · +
+                · · ·`);
+            } else if (Y > anchorY && X < anchorX) {
+                console.log(`
+                · · ·
+                · · ·
+                + · ·`);
+            } else if (Y > anchorY && centerX) {
+                console.log(`
+                · · ·
+                · · ·
+                · + ·`);
+            } else if (Y > anchorY && X > anchorX) {
+                console.log(`
+                · · ·
+                · · ·
+                · · +`);
+            }
+        }
+    };
+
+    const handleActive = () => {
+        setActive(true);
+        // CARD.current?.addEventListener("mousemove", listener);
+    };
+
+    const handleInactive = () => {
+        setActive(false);
+        // CARD.current?.removeEventListener("mousemove", () => {});
+    };
+
     return (
-        <div className="card" onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)}>
-            <p className="name">{name}</p>
-            <p className="number">{number}</p>
-            <img src={image} />
-            <p className="type">{type}</p>
-        </div>
+        <>
+            <div ref={POINTER} className={`pointer ${active ? "pointer-visible" : ""}`}></div>
+            <div
+                ref={CARD}
+                className={`card ${active ? "active" : ""}`}
+                onMouseEnter={handleActive}
+                onMouseLeave={handleInactive}
+                onMouseMove={listener}
+            >
+                <p className="name">{name}</p>
+                <p className="number">{number}</p>
+                <img src={image} />
+                <p className="type">{type}</p>
+            </div>
+        </>
     );
 };
 
