eb2cacb5cc58ccab58ea3f1217b0173a557e19b7
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2024-01-23 07:29:59 -0300

    glow effect working

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,55 @@
 import "./App.css";
 import Card from "./Card";
 import cereludge from "./assets/imgs/Ceruledge.png";
+import armarouge from "./assets/imgs/Armarouge.png";
+import charizard from "./assets/imgs/MegaCharizardY.png";
+import rayquaza from "./assets/imgs/Rayquaza.png";
+import groudon from "./assets/imgs/Groudon.png";
 
 function App() {
     const ceru = {
         name: "Ceruledge",
+        jp: "ソウブレイズ",
         number: "#937",
         image: cereludge,
         type: "Fuego/Fantasma",
     };
+    const arma = {
+        name: "Armarouge",
+        jp: "グレンアルマ",
+        number: "#936",
+        image: armarouge,
+        type: "Fuego/Psíquico",
+    };
+    const rayq = {
+        name: "Rayquaza",
+        jp: "レックウザ",
+        number: "#384",
+        image: rayquaza,
+        type: "Dragon/Volador",
+    };
+    const grou = {
+        name: "Groudon",
+        jp: "グラードン",
+        number: "#383",
+        image: groudon,
+        type: "Tierra",
+    };
+    const chari = {
+        name: "Charizard",
+        jp: "リザードン",
+        number: "#006",
+        image: charizard,
+        type: "Fuego/Dragon",
+    };
     return (
         <>
-            <h1>Cards</h1>
-            <section>
+            <section className="cardsContainer">
                 <Card data={ceru} />
+                <Card data={arma} />
+                <Card data={chari} />
+                <Card data={rayq} />
+                <Card data={grou} />
             </section>
         </>
     );
--- src/Card.tsx
+++ src/Card.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent, useRef, useState, MouseEvent } from "react";
 
 interface Data {
     name: string;
+    jp: string;
     number: string;
     image: string;
     type: string;
@@ -12,188 +13,115 @@ interface Props {
 }
 
 interface iCoords {
-    maxHypot: number;
-    anchorX: number;
-    anchorY: number;
+    maxHypot?: number;
+    anchorX?: number;
+    anchorY?: number;
     width: number;
     height: number;
-    // debug
-    minCenterX: number;
-    minCenterY: number;
-    maxCenterX: number;
-    maxCenterY: number;
+    left: number;
+    top: number;
 }
 
-const logPos = (
-    X: number,
-    Y: number,
-    lastZone: number | null,
-    centerX: boolean,
-    centerY: boolean,
-    minCenterX: number,
-    minCenterY: number,
-    maxCenterX: number,
-    maxCenterY: number
-): number | null => {
-    let pos = null;
-    if (centerY && centerX) {
-        if (lastZone !== 0) {
-            pos = 0;
-            console.clear();
-            console.log(`
-                · · ·
-                · + ·
-                · · ·`);
-        }
-    } else if (Y <= minCenterY && X <= minCenterX) {
-        if (lastZone !== 1) {
-            pos = 1;
-            console.clear();
-            console.log(`
-                + · ·
-                · · ·
-                · · ·`);
-        }
-    } else if (Y <= minCenterY && centerX) {
-        if (lastZone !== 2) {
-            pos = 2;
-            console.clear();
-            console.log(`
-                · + ·
-                · · ·
-                · · ·`);
-        }
-    } else if (Y <= minCenterY && X >= maxCenterX) {
-        if (lastZone !== 3) {
-            pos = 3;
-            console.clear();
-            console.log(`
-                · · +
-                · · ·
-                · · ·`);
-        }
-    } else if (centerY && X <= minCenterX) {
-        if (lastZone !== 4) {
-            pos = 4;
-            console.clear();
-            console.log(`
-                · · ·
-                + · ·
-                · · ·`);
-        }
-    } else if (centerY && X >= maxCenterX) {
-        if (lastZone !== 5) {
-            pos = 5;
-            console.clear();
-            console.log(`
-                · · ·
-                · · +
-                · · ·`);
-        }
-    } else if (Y >= maxCenterY && X <= minCenterX) {
-        if (lastZone !== 6) {
-            pos = 6;
-            console.clear();
-            console.log(`
-                · · ·
-                · · ·
-                + · ·`);
-        }
-    } else if (Y >= maxCenterY && centerX) {
-        if (lastZone !== 7) {
-            pos = 7;
-            console.clear();
-            console.log(`
-                · · ·
-                · · ·
-                · + ·`);
-        }
-    } else if (Y >= maxCenterY && X >= maxCenterX) {
-        if (lastZone !== 8) {
-            pos = 8;
-            console.clear();
-            console.log(`
-                · · ·
-                · · ·
-                · · +`);
-        }
-    }
-    return pos;
-};
-
 const Card: FunctionComponent<Props> = ({ data }) => {
-    const { name, number, image, type } = data;
+    const { name, jp, number, image, type } = data;
     const CARD = useRef<HTMLDivElement>(null);
+    const TTO = useRef<number | null>(null);
+    const GLARE = useRef<HTMLDivElement>(null);
+    const CHROMASHINE = useRef<HTMLDivElement>(null);
+    const CHROMA = useRef<HTMLDivElement>(null);
+
     const [active, setActive] = useState<boolean>(false);
     const [coords, setCoords] = useState<iCoords | null>(null);
-    const sensivity: number = 10; // máximos deg para el rotate
-    // Debug
-    const debug: boolean = false;
-    const POINTER = useRef<HTMLDivElement>(null);
-    const [lastZone, setLastZone] = useState<number | null>(null);
+    const glare: boolean = false;
+    const max: number = 15; // máximos deg para el rotate
+    const perspective: number = 1000;
+    const scale: number = 1.05;
+    const speed: number = 500;
+    const glareOpacity: number = 0.2;
+    const chromaOpacity: number = 0.2;
+
+    const setTransition = () => {
+        TTO.current && clearTimeout(TTO.current);
+        CARD.current && (CARD.current.style.transition = `${speed}ms cubic-bezier(.03,.98,.52,.99)`);
+        glare && GLARE.current && (GLARE.current.style.transition = `opacity ${speed}ms cubic-bezier(.03,.98,.52,.99)`);
+
+        TTO.current = setTimeout(() => {
+            CARD.current && (CARD.current.style.transition = "");
+            glare && GLARE.current && (GLARE.current.style.transition = "");
+        }, speed);
+    };
 
     const listener = (ev: MouseEvent) => {
         if (active && coords && CARD.current) {
-            const { clientY: Y, clientX: X } = ev;
-            const { anchorX, anchorY, maxHypot, width, height, minCenterX, maxCenterX, minCenterY, maxCenterY } =
-                coords;
+            const { clientY, clientX } = ev;
+            const { width, height, left, top } = coords;
 
-            // calcula la distancia del cursor al centro de la carta (hipotenusa)
-            // y la traduce a grados, siendo el centro 0º y la distancia máxima el máximo de grados (definido por la variable sensivity)
-            const hyp = Math.round((Math.hypot(Y - anchorY, X - anchorX) * sensivity) / maxHypot);
-            const xAxis = ((X - anchorX) * width) / 1000000;
-            const yAxis = ((Y - anchorY) * height) / 1000000;
+            // Convierte la coord a un decimal entre 0 y 1
+            // 0 inicio de la card, 1 final
+            const X = (clientX - left) / width;
+            const Y = (clientY - top) / height;
+            // const angle = Math.atan2(clientX - (left + width / 2), -(clientY - (top + height / 2))) * (180 / Math.PI);
 
-            CARD.current.style.rotate = `${xAxis - 1} ${yAxis - 1} 0 ${hyp}deg`;
+            // Calcula angulo en base a la coord
+            // X * max = "porcentaje" del angulo - ej: 0.75 * 15 = 11.25
+            // 10 * 2 = 22.5
+            // 15 - 20 = -7.5deg
+            // Con el cursor al 75% de la card (horizontalmente)
+            // el angulo de inclinación es -7.5 (se eleva el lado derecho)
+            const tiltX = (max - X * max * 2).toFixed(2);
+            const tiltY = (Y * max * 2 - max).toFixed(2);
 
-            if (debug) {
-                const centerX = X > minCenterX && X < maxCenterX;
-                const centerY = Y > minCenterY && Y < maxCenterY;
+            const style = `perspective(${perspective}px) rotateX(${tiltY}deg) rotateY(${tiltX}deg) scale3d(${scale}, ${scale}, ${scale})`;
 
-                const pos = logPos(X, Y, lastZone, centerX, centerY, minCenterX, minCenterY, maxCenterX, maxCenterY);
-                pos !== null && pos !== lastZone && setLastZone(pos);
+            CARD.current.style.transform = style;
+            if (GLARE.current && CHROMA.current && CHROMASHINE.current) {
+                const gX = Number(tiltX) * 12;
+                const gY = Number(tiltY) * 10;
+
+                // Opacidad relativo al angulo Y
+                GLARE.current.style.opacity = `${(Y * 100 * glareOpacity) / 100}`;
+                CHROMA.current.style.opacity = `${(Y * 100 * chromaOpacity) / 100}`;
+                CHROMASHINE.current.style.opacity = `${(Y * 100 * chromaOpacity) / 100}`;
+
+                // Movimiento del reflejo
+                // GLARE.current.style.transform = `translate(${tiltX}%, ${tiltY}%)`;
+                GLARE.current.style.backgroundPositionX = `${gX + 50}%`;
+                GLARE.current.style.backgroundPositionY = `${gY}%`;
+                CHROMA.current.style.backgroundPositionX = `${gX}%`;
+                CHROMA.current.style.backgroundPositionY = `${gY}%`;
+                CHROMASHINE.current.style.backgroundPositionX = `${gX}%`;
+                CHROMASHINE.current.style.backgroundPositionY = `${gY}%`;
             }
         }
     };
 
     const handleActive = () => {
         setActive(true);
-        if (POINTER.current && CARD.current) {
+        if (CARD.current) {
             const { offsetTop, offsetLeft, offsetHeight, offsetWidth } = CARD.current;
-            const anchorX = offsetLeft + offsetWidth / 2;
-            const anchorY = offsetTop + offsetHeight / 2;
-
-            POINTER.current.style.left = anchorX + "px";
-            POINTER.current.style.top = anchorY + "px";
-
-            const tX = 50,
-                tY = 75;
-
             setCoords({
-                maxHypot: Math.round(Math.hypot(offsetHeight / 2, offsetWidth / 2)),
-                anchorX,
-                anchorY,
                 width: offsetWidth,
                 height: offsetHeight,
-                // Debug
-                minCenterX: Math.round(anchorX - tX),
-                maxCenterX: Math.round(anchorX + tX),
-                minCenterY: Math.round(anchorY - tY),
-                maxCenterY: Math.round(anchorY + tY),
+                left: offsetLeft,
+                top: offsetTop,
             });
+            setTransition();
         }
-        // CARD.current?.addEventListener("mousemove", listener);
     };
 
     const handleInactive = () => {
         setActive(false);
-        CARD.current && (CARD.current.style.rotate = `0 0 0 0deg`);
-        // CARD.current?.removeEventListener("mousemove", () => {});
+        if (CARD.current && GLARE.current && CHROMA.current && CHROMASHINE.current) {
+            setTransition();
+            CARD.current.style.transform = `rotateX(0deg) rotateY(0deg)`;
+            GLARE.current.style.opacity = `0`;
+            CHROMASHINE.current.style.opacity = `0`;
+            CHROMA.current.style.opacity = `0`;
+        }
     };
 
     return (
         <>
-            <div ref={POINTER} className={`pointer ${active ? "pointer-visible" : ""}`}></div>
             <div
                 ref={CARD}
                 className={`card ${active ? "active" : ""}`}
@@ -201,10 +129,20 @@ const Card: FunctionComponent<Props> = ({ data }) => {
                 onMouseLeave={handleInactive}
                 onMouseMove={listener}
             >
-                <p className="name">{name}</p>
-                <p className="number">{number}</p>
+                <div ref={GLARE} className={`glare`}></div>
+
+                <div ref={CHROMASHINE} className={`chroma`}></div>
+                <div
+                    ref={CHROMA}
+                    className={`chroma-mask`}
+                    style={{ maskImage: `url(${image})`, WebkitMaskImage: `url(${image})` }}
+                ></div>
                 <img src={image} />
+
                 <p className="type">{type}</p>
+                <p className="name">{name}</p>
+                <p className="namejp">{jp}</p>
+                <p className="number">{number}</p>
             </div>
         </>
     );
